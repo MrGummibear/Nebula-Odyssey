a9d0ae1fd2c97e7c91d4f020809f6034359d45d4
Marius <webermarius1995@gmail.com>
2024-07-19 12:13:30 +0200

    funktionalität änderung der Bilder im werften-title-div

--- Frontend/src/pages/Shipyard/Shipyard.jsx
+++ Frontend/src/pages/Shipyard/Shipyard.jsx
@@ -5,6 +5,9 @@ const Shipyard = () => {
   const [shipsSmall, setShipsSmall] = useState([]);
   const [shipsMedium, setShipsMedium] = useState([]);
   const [shipsBig, setShipsBig] = useState([]);
+  const [shipImage, setShipImage] = useState(
+    `url(/werften/uebersicht-title.png)`
+  );
 
   const werftTypen = {
     klein: [
@@ -12,37 +15,125 @@ const Shipyard = () => {
         id: "kleinerTransporter",
         class: "btn btn1",
         label: "Kleiner Transporter",
+        img: "/werften/kleine_werft/kleiner_transporter/kleiner_transporter1.png",
       },
-      { id: "leichterJaeger", class: "btn btn2", label: "Leichter Jaeger" },
-      { id: "schwererJaeger", class: "btn btn3", label: "Schwerer Jaeger" },
-      { id: "bomber", class: "btn btn4", label: "Bomber" },
-      { id: "fregatte", class: "btn btn5", label: "Fregatte" },
-      { id: "miningDrone", class: "btn btn6", label: "Mining Drone" },
-      { id: "flaggeschuetz", class: "btn btn7", label: "Flaggeschütz" },
-      { id: "lasergeschuetz", class: "btn btn8", label: "Lasergeschütz" },
+      {
+        id: "leichterJaeger",
+        class: "btn btn2",
+        label: "Leichter Jaeger",
+        img: "/werften/kleine_werft/leichter_jaeger/leichter_jaeger1.png",
+      },
+      {
+        id: "schwererJaeger",
+        class: "btn btn3",
+        label: "Schwerer Jaeger",
+        img: "/werften/kleine_werft/schwerer_jaeger/schwerer_jaeger1.png",
+      },
+      {
+        id: "bomber",
+        class: "btn btn4",
+        label: "Bomber",
+        img: "/werften/kleine_werft/bomber/bomber_1.png",
+      },
+      {
+        id: "fregatte",
+        class: "btn btn5",
+        label: "Fregatte",
+        img: "/werften/kleine_werft/fregatte/fregatte_1.png",
+      },
+      {
+        id: "miningDrone",
+        class: "btn btn6",
+        label: "Mining Drone",
+        img: "/werften/kleine_werft/mining_drone/mining_drone1.png",
+      },
+      {
+        id: "flaggeschuetz",
+        class: "btn btn7",
+        label: "Flaggeschütz",
+        img: "/werften/kleine_werft/flaggeschütz/flaggeschütz1.png",
+      },
+      {
+        id: "lasergeschuetz",
+        class: "btn btn8",
+        label: "Lasergeschütz",
+        img: "/werften/kleine_werft/lasergeschütz/lasergeschütz1.jpg",
+      },
     ],
     mittel: [
       {
         id: "grosserTransporter",
         class: "btn btn1",
         label: "Großer Transporter",
+        img: "/werften/mittlere_werft/großer_transporter/großer_transporter1.png",
       },
-      { id: "zerstoerer", class: "btn btn2", label: "Zerstörer" },
-      { id: "kreuzer", class: "btn btn3", label: "Kreuzer" },
-      { id: "flugdeckkreuzer", class: "btn btn4", label: "Flugdeckkreuzer" },
-      { id: "kolonieschiff", class: "btn btn5", label: "Kolonieschiff" },
-      { id: "bergbauschiff", class: "btn btn6", label: "Bergbauschiff" },
-      { id: "ionenkanone", class: "btn btn7", label: "Ionenkanone" },
-      { id: "railgun", class: "btn btn8", label: "Railgun" },
+      {
+        id: "zerstoerer",
+        class: "btn btn2",
+        label: "Zerstörer",
+        img: "/werften/mittlere_werft/zerstörer/zerstörer_1.png",
+      },
+      {
+        id: "kreuzer",
+        class: "btn btn3",
+        label: "Kreuzer",
+        img: "/werften/mittlere_werft/kreuzer/kreuzer_1.png",
+      },
+      {
+        id: "flugdeckkreuzer",
+        class: "btn btn4",
+        label: "Flugdeckkreuzer",
+        img: "/werften/mittlere_werft/flugdeckkreuzer/flugdeckkreuzer_1.png",
+      },
+      {
+        id: "kolonieschiff",
+        class: "btn btn5",
+        label: "Kolonieschiff",
+        img: "/werften/mittlere_werft/kolonieschiff/kolonieschiff_1.png",
+      },
+      {
+        id: "bergbauschiff",
+        class: "btn btn6",
+        label: "Bergbauschiff",
+        img: "/werften/mittlere_werft/bergbauschiff/bergbauschiff_1.png",
+      },
+      {
+        id: "ionenkanone",
+        class: "btn btn7",
+        label: "Ionenkanone",
+        img: "/werften/mittlere_werft/ionenkanone/ionenkanone1.jpg",
+      },
+      {
+        id: "railgun",
+        class: "btn btn8",
+        label: "Railgun",
+        img: "/werften/mittlere_werft/railgun/railgun_1.png",
+      },
     ],
     gross: [
-      { id: "schlachtschiff", class: "btn btn9", label: "Schlachtschiff" },
-      { id: "traegerschiff", class: "btn btn10", label: "Trägerschiff" },
-      { id: "schlachtkreuzer", class: "btn btn11", label: "Schlachtkreuzer" },
+      {
+        id: "schlachtschiff",
+        class: "btn btn9",
+        label: "Schlachtschiff",
+        img: "/werften/große_werft/schlachtschiff/schlachtschiff_1.png",
+      },
+      {
+        id: "traegerschiff",
+        class: "btn btn10",
+        label: "Trägerschiff",
+        img: "/werften/große_werft/traegerschiff/traegerschiff_1.png",
+      },
+      {
+        id: "schlachtkreuzer",
+        class: "btn btn11",
+        label: "Schlachtkreuzer",
+        img: "/werften/große_werft/schlachtkreuzer/schlachtkreuzer_1.png",
+      },
       {
         id: "partikelgeschuetz",
         class: "btn btn12",
         label: "Partikelgeschütz",
+        img: "/werften/große_werft/partikelgeschütz/partikelgeschütz1.jpg",
       },
     ],
   };
@@ -65,9 +156,17 @@ const Shipyard = () => {
     setShipsBig(werftTypen.gross);
   };
 
+  const changeImage = (img) => {
+    setShipImage(`url(${img})`);
+  };
+
   return (
     <div className="content-box">
-      <div id="werften-title" className="werften-title"></div>
+      <div
+        id="werften-title"
+        className="werften-title"
+        style={{ backgroundImage: shipImage }}
+      ></div>
       <div className="werften-box">
         <div className="werft">
           <div className="werft-bar">
@@ -92,17 +191,32 @@ const Shipyard = () => {
           </div>
           <div className="btn-box" id="btn-content">
             {shipsSmall.map((ship) => (
-              <button key={ship.id} id={ship.id} className={ship.class}>
+              <button
+                key={ship.id}
+                id={ship.id}
+                className={ship.class}
+                onClick={() => changeImage(ship.img)}
+              >
                 {ship.label}
               </button>
             ))}
             {shipsMedium.map((ship) => (
-              <button key={ship.id} id={ship.id} className={ship.class}>
+              <button
+                key={ship.id}
+                id={ship.id}
+                className={ship.class}
+                onClick={() => changeImage(ship.img)}
+              >
                 {ship.label}
               </button>
             ))}
             {shipsBig.map((ship) => (
-              <button key={ship.id} id={ship.id} className={ship.class}>
+              <button
+                key={ship.id}
+                id={ship.id}
+                className={ship.class}
+                onClick={() => changeImage(ship.img)}
+              >
                 {ship.label}
               </button>
             ))}
