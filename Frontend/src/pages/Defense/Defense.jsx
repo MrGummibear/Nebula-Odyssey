d75c2c304ab037d740450f7b3c25594cfb73f4ce
Marius <webermarius1995@gmail.com>
2024-07-22 14:12:13 +0200

    defensejsx und css grundkonzept

--- Frontend/src/pages/Defense/Defense.jsx
+++ Frontend/src/pages/Defense/Defense.jsx
@@ -1,11 +1,32 @@
-
+import "./Defense.css";
+import { useState } from "react";
 
 const Defense = () => {
-    return (
-        <div className="content-box">
-            defensly
-        </div>
-    );
-}
+  const [ships, setShips] = useState([]);
+  const [shipImage, setShipImage] = useState(
+    `url(/werften/uebersicht-title.png)`
+  );
+
+  const handleShipType = (type) => {
+    setShips(werftTypen[type]);
+  };
+
+  const changeImage = (img) => {
+    setShipImage(`url(${img})`);
+  };
+
+  return (
+    <div className="content-box">
+      <div className="defense-top">
+        <div className="defense-info"></div>
+        <div className="defense-img"></div>
+      </div>
+      <div className="defense-bot">
+        <div className="defense-btnbox"></div>
+        <div className="defense-description"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Defense;
