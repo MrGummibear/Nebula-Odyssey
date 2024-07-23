f54c2dc810a53a04ed3b63906e4b9a54059eb880
Dennis <mayer.dennis90@gmx.de>
2024-07-23 16:00:58 +0200

    tool tip entfernt

--- Frontend/src/components/Ressourcen/Ressourcebar.jsx
+++ Frontend/src/components/Ressourcen/Ressourcebar.jsx
@@ -1,5 +1,4 @@
 import "./Ressourcebar.css"
-import ImageWithTooltip from './ImageWithTooltip';
 
 const Ressourcebar = () => {
     return (
@@ -7,37 +6,37 @@ const Ressourcebar = () => {
             <div className="ressources">
           <div className="raw-material">
             <div className="raw raw1">
-              <ImageWithTooltip src="/ressource-icons/SILICA.png" alt="SILICA" />
+              <img src="/ressource-icons/SILICA.png" alt="SILICA" />
               <p className="ressource-counter">0000</p>
             </div>
             <div className="raw raw2">
-              <ImageWithTooltip src="/ressource-icons/ORE.png" alt="ORE" />
+              <img src="/ressource-icons/ORE.png" alt="ORE" />
               <p className="ressource-counter">0000</p>
             </div>
             <div className="raw raw3">
-              <ImageWithTooltip src="/ressource-icons/CHEM.png" alt="CHEM" />
+              <img src="/ressource-icons/CHEM.png" alt="CHEM" />
               <p className="ressource-counter">0000</p>
             </div>
           </div>
           <div className="products">
             <div className="prod prod3">
-              <ImageWithTooltip src="/ressource-icons/FUEL.png" alt="FUEL" />
+              <img src="/ressource-icons/FUEL.png" alt="FUEL" />
               <p className="ressource-counter">0000</p>
             </div>
             <div className="prod prod1">
-              <ImageWithTooltip src="/ressource-icons/AMMO.png" alt="AMMO" />
+              <img src="/ressource-icons/AMMO.png" alt="AMMO" />
               <p className="ressource-counter">0000</p>
             </div>
             <div className="prod prod4">
-              <ImageWithTooltip src="/ressource-icons/STEEL.png" alt="STEEL" />
+              <img src="/ressource-icons/STEEL.png" alt="STEEL" />
               <p className="ressource-counter">0000</p>
             </div>
             <div className="prod prod2">
-              <ImageWithTooltip src="/ressource-icons/ELECTRONICS.png" alt="ELECTRONICS" />
+              <img src="/ressource-icons/ELECTRONICS.png" alt="ELECTRONICS" />
               <p className="ressource-counter">0000</p>
             </div>
             <div className="prod prod5">
-              <ImageWithTooltip src="/ressource-icons/ENERGY.png" alt="ENERGY" />
+              <img src="/ressource-icons/ENERGY.png" alt="ENERGY" />
               <p className="ressource-counter">0000</p>
             </div>
           </div>
