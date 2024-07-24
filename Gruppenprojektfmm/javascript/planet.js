f1aec71d416d5ac60520737fab2f3893c0f08650
Dennis <mayer.dennis90@gmx.de>
2024-07-24 20:56:49 +0200

    reinigung

--- Gruppenprojektfmm/javascript/planet.js
+++
@@ -1,7 +0,0 @@
-/*
-benötigt alle gebaeudestufen die auf dem planeten vorhanden sind
-benötigt alle schiffe die auf dem planeten vorhanden sind
-speichert alle ressourcen die auf dem planeten vorhanden sind
-
-
-*/
--- Gruppenprojektfmm/javascript/shipyard.js
+++
@@ -1,7 +0,0 @@
-/*  
-benötigt funktion um schiffe zu bauen 
-handelt die bauzeit
-wird vom playermanager benutzt um schiffe zu bauen, der playermanager verwendet funktionen die hier drin sind
-
-
-*/
