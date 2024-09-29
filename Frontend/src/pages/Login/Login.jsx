b9e256ba06f074f6e85b6e21480cd48da5ca7196
Mynchro <webermarius1995@gmail.com>
2024-09-29 19:43:44 +0200

    login linter, signup jsx und css erstellt

--- Frontend/src/pages/Login/Login.jsx
+++ Frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
-import { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { PlayerContext } from '../../context/PlayerContext';
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { PlayerContext } from "../../context/PlayerContext";
 import "./Login.css";
 
 const Login = () => {
@@ -12,56 +12,58 @@ const Login = () => {
   const handleLogin = (e) => {
     e.preventDefault(); // Verhindert das Standard-Formular-Submit-Verhalten
     playerData.map((currentUser) => {
-        if (currentUser.username === username && currentUser.password === password){
-            setCurrentPlayer({...currentUser})
-        }
-    }) // Daten zum globalen Zustand hinzufügen
+      if (
+        currentUser.username === username &&
+        currentUser.password === password
+      ) {
+        setCurrentPlayer({ ...currentUser });
+      }
+    }); // Daten zum globalen Zustand hinzufügen
     navigate("/overview");
   };
 
-    return (
-        <div className="login-content">
-            <img 
-                className="login-logo" 
-                src="/icons/logo_placeholder.png" 
-                alt="Logo Nebula Odyssey" 
-            />
-            <h1 className="login-title">Nebula Odyssey</h1>
-            <div className="login-box">
-                <h2>Login</h2>
-                <input 
-                    type="text" 
-                    placeholder="Username" 
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <input 
-                    type="password" 
-                    placeholder="Password" 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                          handleLogin(e);
-                        }
-                      }}
-                />
-                <button onClick={handleLogin}>Login</button>
-            </div>
-            <img
-                className="schlachtkreuzer-img" 
-                src="/werften/große_werft/schlachtkreuzer/schlachtkreuzer_1-removebg-preview.png" 
-                alt="schlachtkreuzer" 
-            />
-            <img
-                className="schlachtschiff-img" 
-                src="/werften/große_werft/schlachtschiff/schlachtschiff_1-removebg-preview.png" 
-                alt="schlachtschiff" 
-            />
-        </div>
-    );
+  return (
+    <div className="login-content">
+      <img
+        className="login-logo"
+        src="/icons/logo_placeholder.png"
+        alt="Logo Nebula Odyssey"
+      />
+      <h1 className="login-title">Nebula Odyssey</h1>
+      <div className="login-box">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleLogin(e);
+            }
+          }}
+        />
+        <button onClick={handleLogin}>Login</button>
+        <p>No account? Signup!</p>
+      </div>
+      <img
+        className="schlachtkreuzer-img"
+        src="/werften/große_werft/schlachtkreuzer/schlachtkreuzer_1-removebg-preview.png"
+        alt="schlachtkreuzer"
+      />
+      <img
+        className="schlachtschiff-img"
+        src="/werften/große_werft/schlachtschiff/schlachtschiff_1-removebg-preview.png"
+        alt="schlachtschiff"
+      />
+    </div>
+  );
 };
 
 export default Login;
-
-
